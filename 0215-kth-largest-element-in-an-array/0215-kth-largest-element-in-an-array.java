dbb01fd2da26d210d08ce84bd6828bfcae4da1a7
Rohit Kumar <97820587+rohitkr01@users.noreply.github.com>
2022-12-03 14:24:13 +0530

    Time: 69 ms (48.50%), Space: 51.5 MB (86.02%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,9 +3,22 @@ class Solution {
         
         // Method -1 : Sort the array then return the  (length - k)th element 
         // Time Complexity : O(nlogn) , Space : O(1) .
-        Arrays.sort(nums);
-        return nums[nums.length-k]; 
+        // Arrays.sort(nums);
+        // return nums[nums.length-k]; 
         
-
+        
+        // Method - 2 : using Priority Queue
+        //Time : O(nlogk)
+        if(nums==null || nums.length==0){
+        return 0;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int val : nums){
+            pq.add(val);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
     }
 }
