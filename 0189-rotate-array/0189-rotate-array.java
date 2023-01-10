acedad386d4bc50da7504e3b42db64fb0a5f025f
Rohit Kumar <97820587+rohitkr01@users.noreply.github.com>
2023-01-10 13:07:40 +0530

    Time: 0 ms (100.00%), Space: 58.6 MB (62.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,4 +1,6 @@
 class Solution {
+    
+    // Method - 1 , Time complexity : O(n) , Space complexity: O(1)
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k=k%n;
@@ -15,4 +17,46 @@ class Solution {
             high--;
         }
     }
+   
+    /*
+    // Method - 2 :  Time: O(n) ,Space : O(n)
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        
+        k = k%n;
+        int[] result = new int[n];
+        int j = n-k;
+        for(int i = 0 ;i < k; i++){
+            result[i] = nums[j];
+            j++;
+        }
+        j=0;
+        for(int i= k ; i<n;i++){
+           result[i] = nums[j]; 
+            j++;
+        }
+       for(int i=0;i<n;i++){
+           nums[i]=result[i];
+       } 
+    }
+    */
+    /*
+    // Method - 3 : 
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = n- k;i<n;i++){
+            list.add(nums[i]);
+        }
+        for(int i=0;i<n-k;i++){
+            list.add(nums[i]);
+        }
+        for(int i =0;i< list.size();i++){
+            nums[i]=list.get(i);
+        }
+    }
+    */
 }
