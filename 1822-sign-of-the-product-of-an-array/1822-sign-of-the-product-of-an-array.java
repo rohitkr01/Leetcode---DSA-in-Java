379614a1b85940964253c2f09fb4763a513f21b2
Rohit Kumar <97820587+rohitkr01@users.noreply.github.com>
2023-05-03 02:20:55 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (50.25%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int arraySign(int[] nums) {
-        var sign = 1;
+        int sign = 1;
 
-        for (var n : nums) {
+        for (int n : nums) {
           if (n == 0) return 0;
           sign *= n > 0 ? 1 : -1;
         }
