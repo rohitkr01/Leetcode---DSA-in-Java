aa3977f1f717a6c15097035fdd127e90aea7f177
Rohit Kumar <97820587+rohitkr01@users.noreply.github.com>
2023-07-05 16:08:58 +0530

    Time: 1 ms (99.68%), Space: 46.3 MB (92.77%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,19 +1,31 @@
 class Solution {
-    public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        ans[0]=0;
-        for(int i =1; i <= n ; i++){
-            ans[i] = cntOneBit(i);
-        }
-        return ans;
-    }
     
-    public int cntOneBit(int n){
-        int cntOne = 0;
-        while( n!= 0){
-            cntOne += n & 1;
-            n = n >>> 1;
-        }
-        return cntOne;
+    //Method - 1
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n+1];
+//         ans[0]=0;
+//         for(int i =1; i <= n ; i++){
+//             ans[i] = cntOneBit(i);
+//         }
+//         return ans;
+//     }
+    
+//     public int cntOneBit(int n){
+//         int cntOne = 0;
+//         while( n!= 0){
+//             cntOne += n & 1;
+//             n = n >>> 1;
+//         }
+//         return cntOne;
+//     }
+    
+    // Method -2
+    // An easy recurrence for this problem is f[i] = f[i / 2] + i % 2.
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i=1; i<=n; i++) 
+            ans[i] = ans[i >> 1] + (i & 1);
+        
+        return ans;
     }
 }
